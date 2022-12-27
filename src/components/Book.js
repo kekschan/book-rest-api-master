51e8f7385ef32ed1ffdb9bbde2040678e4ec1767
Denis Borshev <dnsborshev@gmail.com>
2022-12-27 13:02:23 +0300

    сделали кнопку обновления на Book.js и реализовали post-запрос

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,60 +1,79 @@
 import React, {Component} from 'react';
 import {Button, Card, Col, Form} from "react-bootstrap";
-import { faPlusSquare, faSave} from "@fortawesome/free-solid-svg-icons";
+import {faPlusSquare, faSave, faUndo} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+import axios from "axios";
 
 export default class Book extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            title: '', author: '', isbnNumber: '',
-            price: '', language: '', action: ''
-        };
+        this.state = this.initialState;
         this.bookChange = this.bookChange.bind(this);
         this.submitBook = this.submitBook.bind(this);
 
     }
 
-    submitBook(event) {
-        alert(
-            ' Title: ' + this.state.title +
-            ' Author: ' + this.state.author +
-            ' Isbn: ' + this.state.isbnNumber +
-            ' Price: ' + this.state.price +
-            ' Language: ' + this.state.language +
-            ' Action: ' + this.state.action);
-        event.preventDefault();
+    initialState = {
+        title: '', coverPhotoURL: '', author: '',
+        isbnNumber: '', price: '', language: ''
     }
 
-    bookChange(event) {
+    resetBook = () => {
+        this.setState(() => this.initialState);
+    }
+
+    submitBook = event => {
+        event.preventDefault();
+
+        const book = {
+            title: this.state.title,
+            author: this.state.author,
+            isbnNumber: this.state.isbnNumber,
+            price: this.state.price,
+            language: this.state.language,
+            coverPhotoURL: this.state.coverPhotoURL
+        };
+
+        axios.post("http://localhost:8081/rest/books",book)
+            .then(response => {
+                if(response.data != null){
+                    this.setState(this.initialState);
+                    alert("Книжка сохранилась");
+                }
+            })
+    }
+
+    bookChange = event => {
         this.setState({
             [event.target.name]: event.target.value
         });
     }
 
     render() {
+
+        const {title, author, isbnNumber, price, language, coverPhotoURL} = this.state;
+
         return (
             <Card className={"border border-dark bg-dark text-white"}>
                 <Card.Header> <FontAwesomeIcon icon={faPlusSquare}/> Добавить книгу</Card.Header>
-                <Form onSubmit={this.submitBook} id={"bookFormId"}>
+                <Form onReset={this.resetBook} onSubmit={this.submitBook} id={"bookFormId"}>
                     <Card.Body>
                         <Form.Row>
                             <Form.Group as={Col} controlId={"formGridTitle"}>
                                 <Form.Label>Название</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete={"off"}
                                               type="text" name={"title"}
-                                              value={this.state.title}
-                                              onChange={this.bookChange}
+                                              value={title} onChange={this.bookChange}
                                               className={"bg-dark text-white"}
                                               placeholder="Введите название книги"/>
                             </Form.Group>
                             <Form.Group as={Col} controlId={"formGridAuthor"}>
                                 <Form.Label>Автор</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete={"off"}
                                               type="text" name={"author"}
-                                              value={this.state.author}
-                                              onChange={this.bookChange}
+                                              value={author} onChange={this.bookChange}
                                               className={"bg-dark text-white"}
                                               placeholder="Введите автора"/>
                             </Form.Group>
@@ -62,19 +81,17 @@ export default class Book extends Component {
                         <Form.Row>
                             <Form.Group as={Col} controlId={"formGridIsbm"}>
                                 <Form.Label>ISBN номер</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete={"off"}
                                               type="text" name={"isbnNumber"}
-                                              value={this.state.isbnNumber}
-                                              onChange={this.bookChange}
+                                              value={isbnNumber} onChange={this.bookChange}
                                               className={"bg-dark text-white"}
                                               placeholder="Введите ISBN номер"/>
                             </Form.Group>
                             <Form.Group as={Col} controlId={"formGridPrice"}>
                                 <Form.Label>Цена</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete={"off"}
                                               type="text" name={"price"}
-                                              value={this.state.price}
-                                              onChange={this.bookChange}
+                                              value={price} onChange={this.bookChange}
                                               className={"bg-dark text-white"}
                                               placeholder="Введите цену"/>
                             </Form.Group>
@@ -82,27 +99,28 @@ export default class Book extends Component {
                         <Form.Row>
                             <Form.Group as={Col} controlId={"formGridLanguage"}>
                                 <Form.Label>Язык</Form.Label>
-                                <Form.Control required
+                                <Form.Control required autoComplete={"off"}
                                               type="text" name={"language"}
-                                              value={this.state.language}
-                                              onChange={this.bookChange}
+                                              value={language} onChange={this.bookChange}
                                               className={"bg-dark text-white"}
                                               placeholder="Введите язык"/>
                             </Form.Group>
-                            <Form.Group as={Col} controlId={"formGridAction"}>
-                                <Form.Label>Жанр</Form.Label>
-                                <Form.Control required
-                                              type="text" name={"action"}
-                                              value={this.state.action}
-                                              onChange={this.bookChange}
+                            <Form.Group as={Col} controlId={"formCoverPhotoURL"}>
+                                <Form.Label>URL </Form.Label>
+                                <Form.Control required autoComplete={"off"}
+                                              type="text" name={"coverPhotoURL"}
+                                              value={coverPhotoURL} onChange={this.bookChange}
                                               className={"bg-dark text-white"}
-                                              placeholder="Введите жанр"/>
+                                              placeholder="Введите адрес картинки"/>
                             </Form.Group>
                         </Form.Row>
                     </Card.Body>
                     <Card.Footer style={{"textAlign": "right"}}>
                         <Button size={"sm"} variant="success" type="submit">
                             <FontAwesomeIcon icon={faSave}/> Добавить
+                        </Button>{' '}
+                        <Button size={"sm"} variant="info" type="reset">
+                            <FontAwesomeIcon icon={faUndo}/> Обновить
                         </Button>
                     </Card.Footer>
                 </Form>
