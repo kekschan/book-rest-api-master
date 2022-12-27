03e0c24727345e5092e123a0f338ba33dbb5d4fa
Denis Borshev <dnsborshev@gmail.com>
2022-12-27 13:53:26 +0300

    добавили сплывающее окно, но проблема с местоположением

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,12 +4,14 @@ import {faPlusSquare, faSave, faUndo} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 import axios from "axios";
+import MyToast from "./MyToast";
 
 export default class Book extends Component {
 
     constructor(props) {
         super(props);
         this.state = this.initialState;
+        this.state.show = false;
         this.bookChange = this.bookChange.bind(this);
         this.submitBook = this.submitBook.bind(this);
 
@@ -39,10 +41,13 @@ export default class Book extends Component {
         axios.post("http://localhost:8081/rest/books",book)
             .then(response => {
                 if(response.data != null){
-                    this.setState(this.initialState);
-                    alert("Книжка сохранилась");
+                    this.setState({"show": true});
+                    setTimeout(() =>  this.setState({"show": false}), 3000);
+                } else {
+                    this.setState({"show": false});
                 }
-            })
+            });
+        this.setState(this.initialState);
     }
 
     bookChange = event => {
@@ -56,75 +61,80 @@ export default class Book extends Component {
         const {title, author, isbnNumber, price, language, coverPhotoURL} = this.state;
 
         return (
-            <Card className={"border border-dark bg-dark text-white"}>
-                <Card.Header> <FontAwesomeIcon icon={faPlusSquare}/> Добавить книгу</Card.Header>
-                <Form onReset={this.resetBook} onSubmit={this.submitBook} id={"bookFormId"}>
-                    <Card.Body>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId={"formGridTitle"}>
-                                <Form.Label>Название</Form.Label>
-                                <Form.Control required autoComplete={"off"}
-                                              type="text" name={"title"}
-                                              value={title} onChange={this.bookChange}
-                                              className={"bg-dark text-white"}
-                                              placeholder="Введите название книги"/>
-                            </Form.Group>
-                            <Form.Group as={Col} controlId={"formGridAuthor"}>
-                                <Form.Label>Автор</Form.Label>
-                                <Form.Control required autoComplete={"off"}
-                                              type="text" name={"author"}
-                                              value={author} onChange={this.bookChange}
-                                              className={"bg-dark text-white"}
-                                              placeholder="Введите автора"/>
-                            </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId={"formGridIsbm"}>
-                                <Form.Label>ISBN номер</Form.Label>
-                                <Form.Control required autoComplete={"off"}
-                                              type="text" name={"isbnNumber"}
-                                              value={isbnNumber} onChange={this.bookChange}
-                                              className={"bg-dark text-white"}
-                                              placeholder="Введите ISBN номер"/>
-                            </Form.Group>
-                            <Form.Group as={Col} controlId={"formGridPrice"}>
-                                <Form.Label>Цена</Form.Label>
-                                <Form.Control required autoComplete={"off"}
-                                              type="text" name={"price"}
-                                              value={price} onChange={this.bookChange}
-                                              className={"bg-dark text-white"}
-                                              placeholder="Введите цену"/>
-                            </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId={"formGridLanguage"}>
-                                <Form.Label>Язык</Form.Label>
-                                <Form.Control required autoComplete={"off"}
-                                              type="text" name={"language"}
-                                              value={language} onChange={this.bookChange}
-                                              className={"bg-dark text-white"}
-                                              placeholder="Введите язык"/>
-                            </Form.Group>
-                            <Form.Group as={Col} controlId={"formCoverPhotoURL"}>
-                                <Form.Label>URL </Form.Label>
-                                <Form.Control required autoComplete={"off"}
-                                              type="text" name={"coverPhotoURL"}
-                                              value={coverPhotoURL} onChange={this.bookChange}
-                                              className={"bg-dark text-white"}
-                                              placeholder="Введите адрес картинки"/>
-                            </Form.Group>
-                        </Form.Row>
-                    </Card.Body>
-                    <Card.Footer style={{"textAlign": "right"}}>
-                        <Button size={"sm"} variant="success" type="submit">
-                            <FontAwesomeIcon icon={faSave}/> Добавить
-                        </Button>{' '}
-                        <Button size={"sm"} variant="info" type="reset">
-                            <FontAwesomeIcon icon={faUndo}/> Обновить
-                        </Button>
-                    </Card.Footer>
-                </Form>
-            </Card>
+            <div>
+                <div style={{"display": this.state.show ? "block" : "none"}}>
+                    <MyToast children = {{show: this.state.show, message: "Книжка добавлена в библиотеку."}}/>
+                </div>
+                <Card className={"border border-dark bg-dark text-white"}>
+                    <Card.Header> <FontAwesomeIcon icon={faPlusSquare}/> Добавить книгу</Card.Header>
+                    <Form onReset={this.resetBook} onSubmit={this.submitBook} id={"bookFormId"}>
+                        <Card.Body>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId={"formGridTitle"}>
+                                    <Form.Label>Название</Form.Label>
+                                    <Form.Control required autoComplete={"off"}
+                                                  type="text" name={"title"}
+                                                  value={title} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Введите название книги"/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId={"formGridAuthor"}>
+                                    <Form.Label>Автор</Form.Label>
+                                    <Form.Control required autoComplete={"off"}
+                                                  type="text" name={"author"}
+                                                  value={author} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Введите автора"/>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId={"formGridIsbm"}>
+                                    <Form.Label>ISBN номер</Form.Label>
+                                    <Form.Control required autoComplete={"off"}
+                                                  type="text" name={"isbnNumber"}
+                                                  value={isbnNumber} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Введите ISBN номер"/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId={"formGridPrice"}>
+                                    <Form.Label>Цена</Form.Label>
+                                    <Form.Control required autoComplete={"off"}
+                                                  type="text" name={"price"}
+                                                  value={price} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Введите цену"/>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId={"formGridLanguage"}>
+                                    <Form.Label>Язык</Form.Label>
+                                    <Form.Control required autoComplete={"off"}
+                                                  type="text" name={"language"}
+                                                  value={language} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Введите язык"/>
+                                </Form.Group>
+                                <Form.Group as={Col} controlId={"formCoverPhotoURL"}>
+                                    <Form.Label>URL </Form.Label>
+                                    <Form.Control required autoComplete={"off"}
+                                                  type="text" name={"coverPhotoURL"}
+                                                  value={coverPhotoURL} onChange={this.bookChange}
+                                                  className={"bg-dark text-white"}
+                                                  placeholder="Введите адрес картинки"/>
+                                </Form.Group>
+                            </Form.Row>
+                        </Card.Body>
+                        <Card.Footer style={{"textAlign": "right"}}>
+                            <Button size={"sm"} variant="success" type="submit">
+                                <FontAwesomeIcon icon={faSave}/> Добавить
+                            </Button>{' '}
+                            <Button size={"sm"} variant="info" type="reset">
+                                <FontAwesomeIcon icon={faUndo}/> Обновить
+                            </Button>
+                        </Card.Footer>
+                    </Form>
+                </Card>
+            </div>
         )
     }
 }
